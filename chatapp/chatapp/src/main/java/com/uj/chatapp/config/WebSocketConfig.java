3eb017043888876f46e0df2eea1a592d5d07efd2
upendra <joshiupendra1995>
2020-05-21 16:41:04 +0530

    webconfig

--- chatapp/chatapp/src/main/java/com/uj/chatapp/config/WebSocketConfig.java
+++ chatapp/chatapp/src/main/java/com/uj/chatapp/config/WebSocketConfig.java
@@ -1,8 +1,42 @@
 package com.uj.chatapp.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
 /**
  * CreatedBy Upendra
  */
-public class WebSocketConfig {
+
+/*
+ * The @EnableWebSocketMessageBroker is used to enable our WebSocket server. We
+ * implement WebSocketMessageBrokerConfigurer interface and provide
+ * implementation for some of its methods to configure the websocket connection.
+ * 
+ * In the first method, we register a websocket endpoint that the clients will
+ * use to connect to our websocket server.
+ * 
+ * Notice the use of withSockJS() with the endpoint configuration. SockJS is
+ * used to enable fallback options for browsers that don’t support websocket.
+ */
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		registry.addEndpoint("/ws").withSockJS();
+	}
+
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry registry) {
+		registry.setApplicationDestinationPrefixes("/app");
+
+		// Use this for enabling a Full featured broker like RabbitMQ
+		registry.enableStompBrokerRelay("/topic").setRelayHost("157.33.229.214").setRelayPort(5672).setClientLogin("guest1234")
+				.setClientPasscode("guest@123456").setSystemLogin("guest1234").setSystemPasscode("guest@123456");
+	}
 
 }
--- chatapp/chatapp/src/main/java/com/uj/chatapp/listener/WebSocketEventListener.java
+++ chatapp/chatapp/src/main/java/com/uj/chatapp/listener/WebSocketEventListener.java
@@ -1,16 +1,43 @@
 package com.uj.chatapp.listener;
 
-/**
- * The listener interface for receiving webSocketEvent events.
- * The class that is interested in processing a webSocketEvent
- * event implements this interface, and the object created
- * with that class is registered with a component using the
- * component's <code>addWebSocketEventListener<code> method. When
- * the webSocketEvent event occurs, that object's appropriate
- * method is invoked.
- *
- * @see WebSocketEventEvent
- */
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectedEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+import com.uj.chatapp.model.ChatMessage;
+
+@Component
 public class WebSocketEventListener {
 
+	private static final Logger logger = LoggerFactory.getLogger(WebSocketEventListener.class);
+
+	@Autowired
+	private SimpMessageSendingOperations messagingTemplate;
+
+	@EventListener
+	public void handleWebSocketConnectListener(SessionConnectedEvent event) {
+		logger.info("Received a new web socket connection");
+	}
+
+	@EventListener
+	public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+		StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+
+		String username = (String) headerAccessor.getSessionAttributes().get("username");
+		if (username != null) {
+			logger.info("User Disconnected:{} " + username);
+
+			ChatMessage chatMessage = new ChatMessage();
+			chatMessage.setMessageType(ChatMessage.MessageType.LEAVE);
+			chatMessage.setSender(username);
+
+			messagingTemplate.convertAndSend("/topic/public", chatMessage);
+		}
+	}
 }
